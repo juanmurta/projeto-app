ed0a2b16d92975cc0adff014770613f81c6c54fa
juanmurta <juanmurta@gmail.com>
2025-05-14 17:44:47 -0300

    criando conexão com o banco de dados firebase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 import requests
+from bannervenda import BannerVenda
 from telas import *
 from botoes import *
 
@@ -14,9 +15,28 @@ class MainApp(App):
         return GUI
 
     def on_start(self):
+        # pegar informações do usuário
         requisicao = requests.get(f"https://projetoapp-64657-default-rtdb.firebaseio.com/{self.id_usuario}.json")
         requisicao_dic = requisicao.json()
+
+        # preencher foto de perfil
         avatar = requisicao_dic["avatar"]
+        foto_perfil = self.root.ids["fotos_perfil"]
+        foto_perfil.source = f"icones/fotos_perfil/{avatar}"
+
+        # preencher lista de vendas
+        try:
+            vendas = requisicao_dic["vendas"][1:]
+            for venda in vendas:
+                banner = BannerVenda(cliente=venda["cliente"], foto_cliente=venda["foto_cliente"],
+                                     produto=venda["produto"], foto_produto=venda["foto_produto"],
+                                     data=venda["data"], preco=venda["preco"],
+                                     unidade=venda["unidade"], quantidade=venda["quantidade"],)
+                pagina_homepage = self.root.ids["homepage"]
+                lista_vendas = pagina_homepage.ids["lista_vendas"]
+                lista_vendas.add_widget(banner)
+        except:
+            pass
 
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
